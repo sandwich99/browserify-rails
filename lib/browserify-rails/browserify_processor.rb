9e6083f6eb9e1844ecc4a1442a1938835d7398a4
sandwich99 <ziyinghj@gmail.com>
2014-05-08 10:16:42 +0800

    default option

--- lib/browserify-rails/browserify_processor.rb
+++ lib/browserify-rails/browserify_processor.rb
@@ -81,7 +81,6 @@ module BrowserifyRails
     # @return [String] Output on standard out
     def run_browserify(options)
       command = "#{browserify_cmd} #{options}"
-      puts "xxxxxxxxxxxxxxxxxxxxxxxxxx #{command} xxxxxxxxxxxxxxxxxxxxxx"
       directory = File.dirname(file)
       stdout, stderr, status = Open3.capture3(command, stdin_data: data, chdir: directory)
 
@@ -93,7 +92,7 @@ module BrowserifyRails
     end
 
     def options
-      options = []
+      options = ["-t browserify-shim"]
 
       options.push("-d") if config.source_map_environments.include?(Rails.env)
 
